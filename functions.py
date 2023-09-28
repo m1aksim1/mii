fdbf3527d6d0f57f8c517ebad079d75eff10ce9b
m1aksim1 <kosheevmaksim228@gmail.com>
2023-09-28 21:18:51 +0400

    init commit

---
+++ functions.py
@@ -0,0 +1,107 @@
+import csv
+
+def csv_to_html_table(file_name, start_row, end_row, start_column, end_column):
+    with open(file_name, 'r') as file:
+        csv_reader = csv.reader(file)
+        html_table = '<table>'
+        html_thead = '<thead>'
+        html_tbody = '<tbody>'
+
+        # первая строка с названиями столбцов
+        html_thead += '<tr>'
+        for j, element in enumerate(csv_reader.__next__()):
+            if start_column <= j <= end_column:
+                html_thead += f'<th>{element}</th>'
+        html_thead += '</tr>'
+        html_thead += '<tr>'
+
+        counter = [0]*end_column
+        csv_reader2 = csv.reader(file)
+        for i, row in enumerate(csv_reader2):
+            if start_row <= i <= end_row:
+                html_tbody += '<tr>'
+                for j, element in enumerate(row):
+                    if start_column <= j <= end_column:
+                        if start_row == i:
+                            print(element)
+                            html_thead += f'<th>{get_type(element)}</th>'
+                        html_tbody += f'<td>{element}</td>'
+                        if not element:
+                            counter[j] += 1
+                html_tbody += '</tr>'
+        html_thead += '<tr>'
+        for j in range(end_column):
+            if start_column <= j <= end_column:
+                html_thead += f'<td>{str(counter[j])+"/"+str(end_row-start_row+1)}</td>'
+        html_thead += '</tr>'
+        html_table += html_thead
+        html_table += html_tbody
+        html_table += '</table>'
+        print(counter)
+        return html_table
+
+def sort_array(_array, col):
+    sorted_array = _array.copy()
+
+    sorted_array.pop(0)
+    sorted_array = sorted(sorted_array, key=lambda x: x[col])
+    return [_array[0]]+sorted_array
+
+
+def group_by_column(file_path, column_index):
+    groups = {}
+    result = []
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        s = csv_reader.__next__()
+        result.append([s[column_index], 'min', 'max', 'avg', 'count'])
+        for row in csv_reader:
+            column_value = row[column_index]
+            if column_value not in groups:
+                groups[column_value] = []
+            groups[column_value].append(row)
+
+    for key, value in groups.items():
+        group_element = ""
+        costs = []
+        for row in value:
+            group_element = row[column_index]
+            costs.append(float(row[7]))
+
+        result.append([group_element, min(costs), max(costs), int(sum(costs) / len(value)),len(value)])
+
+    return result
+
+
+def array_to_html(arr):
+    html = '<table>'
+    for row in arr:
+        html += '<tr>'
+        for item in row:
+            html += '<td>{}</td>'.format(item)
+        html += '</tr>'
+    html += '</table>'
+    return html
+
+
+def csv_to_array(file_path):
+    array = []
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            array.append(row)
+    return array
+
+
+def get_type(string):
+    try:
+        number = int(string)
+        return "int"
+    except ValueError:
+        pass
+    try:
+        number = float(string)
+        return "float"
+    except ValueError:
+        pass
+    return "string"
---
+++ main.py
@@ -0,0 +1,112 @@
+import os.path
+import random
+
+from flask import Flask, redirect, url_for, request, render_template
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import math
+
+import functions
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return open("main.html", 'r', encoding='utf-8').read()
+
+
+@app.route("/get_table", methods=['GET', 'POST'])
+def get_table():
+    data = request.args
+    start_row: int = int(data['start_row'])
+    end_row: int = int(data['end_row'])
+    start_column: int = int(data['start_column'])
+    end_column: int = int(data['end_column'])
+    # print(csv_to_html_table("Diamonds Prices2022.csv", start_row, end_row, start_column, end_column));
+    return functions.csv_to_html_table("Diamonds Prices2022.csv", start_row, end_row, start_column, end_column)
+
+
+@app.route("/lab2", methods=['GET', 'POST'])
+def lab2():
+
+    #огранка
+    html_table = functions.array_to_html(functions.group_by_column("Diamonds Prices2022.csv", 2))
+
+    #караты
+    my_array = functions.group_by_column("Diamonds Prices2022.csv", 1)
+    html_table += functions.array_to_html(functions.sort_array(my_array, 0))
+
+    #прозрачность
+    html_table += functions.array_to_html(functions.group_by_column("Diamonds Prices2022.csv", 4))
+
+    #цвет
+    html_table += functions.array_to_html(functions.group_by_column("Diamonds Prices2022.csv", 3))
+
+    return html_table
+
+
+@app.route("/lab3", methods=['GET', 'POST'])
+def lab3():
+    group_data = functions.group_by_column("Diamonds Prices2022.csv",1)
+    sort_group_data = functions.sort_array(group_data, 0)
+
+    avg_carat = get_avg_value("Diamonds Prices2022.csv", 1, 4)
+    avg_cut = get_avg_value("Diamonds Prices2022.csv",2,4)
+    avg_color = get_avg_value("Diamonds Prices2022.csv",3,4)
+    avg_clarity = get_avg_value("Diamonds Prices2022.csv", 4, 4)
+    avg_depth = get_avg_value("Diamonds Prices2022.csv", 5, 4)
+    avg_table = get_avg_value("Diamonds Prices2022.csv", 6, 4)
+    avg_price = get_avg_value("Diamonds Prices2022.csv", 7, 4)
+    avg_x = get_avg_value("Diamonds Prices2022.csv", 8, 4)
+    avg_y = get_avg_value("Diamonds Prices2022.csv", 9, 4)
+    avg_z = get_avg_value("Diamonds Prices2022.csv", 10, 4)
+
+
+
+    dpi = 80
+    fig = plt.figure(dpi=dpi, figsize=(5000 / dpi, 1000 / dpi))
+    mpl.rcParams.update({'font.size': 6})
+
+    plt.axis([0, 275, 0, 20000])
+
+    plt.title('Карат & Стоимость')
+    plt.xlabel('Карат')
+    plt.ylabel('Стоимость')
+
+
+    y_array = []
+    x_array = []
+
+    new_y_array = []
+    new_x_array = []
+
+    sort_group_data.pop(0)
+    for element in sort_group_data:
+        y_array += [element[3]]
+        x_array += [element[0]]
+
+
+
+
+    plt.plot(x_array, y_array, color='blue', linestyle='solid', label='исходные данные')
+
+
+    plt.legend(loc='upper right')
+    fig.savefig('static/pics/trigan.png')
+
+
+    picFolder = os.path.join('static', 'pics')
+    app.config['UPLOAD_FOLDER'] = picFolder
+    pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'trigan.png')
+    return render_template("lab3.html", user_image = pic1)
+
+
+def get_avg_value(csv, col, param):
+    group_data = functions.group_by_column(csv, col)
+    sort_group_data_count = functions.sort_array(group_data, param)
+    slice_data = sort_group_data_count[len(sort_group_data_count) - 5:len(sort_group_data_count):1]
+    return slice_data
+
+if __name__ == "__main__":
+    app.run(debug=False)
